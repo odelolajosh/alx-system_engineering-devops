70bf9ba6ad5f644c914d012baa926a65536dedb1
odelolajosh <oladapoodelola@gmail.com>
2022-09-18 21:07:16 +0100

    fix: task 100 - use right path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # configure passwordless ssh login to the remote host
 exec { 'echo':
     path    => '/usr/bin:/bin',
-    command => 'echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> ./ssh_config',
+    command => 'echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> /etc/ssh/ssh_config',
     returns => [0, 1],
 }
