8092a06d872ae7299fbdaa738c0054257cf8e2f7
odelolajosh <oladapoodelola@gmail.com>
2022-09-18 20:58:18 +0100

    feat: task 100 - CM for ssh config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # configure passwordless ssh login to the remote host
-exec { 'echo':
+exec { 'configure ssh_config':
     path    => '/usr/bin:/bin',
     command => 'echo \tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no >> /etc/ssh/ssh_config',
     returns => [0, 1]
