60f2ae6f7a645e5e18b8e0bf14a626c9e9fffe5e
odelolajosh <oladapoodelola@gmail.com>
2022-09-18 21:06:15 +0100

    fix: task 100 - use escape characters

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # configure passwordless ssh login to the remote host
-exec { 'configure ssh_config':
+exec { 'echo':
     path    => '/usr/bin:/bin',
-    command => 'echo \tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no >> /etc/ssh/ssh_config',
-    returns => [0, 1]
+    command => 'echo "\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication no" >> ./ssh_config',
+    returns => [0, 1],
 }
