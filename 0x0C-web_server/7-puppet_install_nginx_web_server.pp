228ef6fba13d25a90d7d6291ab03b5922d3d05e6
odelolajosh <oladapoodelola@gmail.com>
2022-09-20 17:02:48 +0100

    fix: task 7 - remove array

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,5 @@
 # Install nginx web server with puppet
 exec { 'install nginx':
     provider => shell,
-    path     => '/usr/bin:/bin',
     command  => 'sudo apt-get update; sudo apt-get install -y nginx; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a \\\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/odelolajosh;\n\t}" /etc/nginx/sites-available/default; sudo service nginx start'
 }
