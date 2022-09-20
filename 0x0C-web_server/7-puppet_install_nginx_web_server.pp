09bcff363178a5a14feb9487b2cb7c195e2a7165
odelolajosh <oladapoodelola@gmail.com>
2022-09-20 18:36:25 +0100

    fix: task 7 - add fixes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,5 +3,5 @@ exec { 'install nginx':
     provider => shell,
     path     => '/usr/bin:/bin:/usr/sbin:/sbin',
     command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/a \\\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/odelolajosh;\n\t}" /etc/nginx/sites-available/default; sudo service nginx start',
-    returns  => 0,
+    returns  => [0, 1],
 }
