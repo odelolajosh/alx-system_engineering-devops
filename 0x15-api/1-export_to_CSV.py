a6a86a7d4990d59e4e54d784ee86ded31418c660
odelolajosh <oladapoodelola@gmail.com>
2022-10-17 23:59:44 +0100

    feat: export result to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" export TODO list progress for a given employee ID into a CSV file. """
+import requests as request
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    usage = "Usage: ./0-gather_data_from_an_API.py <employee_id>"
+
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError(usage)
+
+        employee_id = int(sys.argv[1])
+        number_of_tasks = 0
+        number_of_done_tasks = 0
+        titles = []
+        employee = request.get(url + "users/{}".format(employee_id)).json()
+        employee_name = employee.get("name")
+        params = {"userId": employee_id}
+        tasks = request.get(url + "todos", params=params).json()
+
+        file = "{}.csv".format(employee_id)
+
+        with open(file, 'x', newline='') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for task in tasks:
+                writer.writerow([employee_id, employee_name,
+                                 task.get("completed"),
+                                 task.get("title")])
+
+    except Exception:
+        exit
